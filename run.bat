dc0a5324938b834dfc5614e0a1491063668ecc66
Mouchi <grosjen@hotmail.fr>
2016-11-07 17:03:30 +0100

    Update the script
    Many changes have been done in the arguments

--- run.bat
+++ run.bat
@@ -13,11 +13,11 @@ mode con cols=80 lines=16
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::::::::: DEBUT VARIABLES DU PROGRAMME JAVA A MODIFIER SI BESOIN :::::::::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: langue (si -fr, le programme est mis en français, sinon il est en anglais)
+:: langue (si -fr, le programme est mis en français, sinon mettre -en pour l'anglais)
 set langue="-fr"
 
 :: carte du jeu
-set carte="vanille_15_9_1066.bmp"
+set carte="vanille_1066_9_15.bmp"
 
 :: fichier de sortie
 set out="res.png"
@@ -25,8 +25,12 @@ set out="res.png"
 :: illustration pendant le chargement
 set illustration="illustration.png"
 
+:: Jeu qui a créé la carte
+:: pour CK2, commentez la ligne suivante
+::set game="-eu4"
+
 :: Dossier du jeu
-set game="C:/Jeux/Steam/SteamApps/common/Crusader Kings II"
+set gameDir="C:/Program Files/Steam/SteamApps/common/Crusader Kings II"
 
 :: Dossier des mods
 ::set mod1="C:\...\Documents\Paradox Interactive\Crusader Kings II\MOD\swmh2.854"
@@ -59,13 +63,19 @@ set dateAn="-yearOnly"
 
 :: La date sera affichée en lettres
 ::set dateText="-textualDate"
+
+:: [CK2] La limitation du rang des titres à écrire
+::set rank="count"
+::set rank="duke"
+::set rank="king"
+set rank="emperor"
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :::::::::: FIN VARIABLES DU PROGRAMME JAVA A MODIFIER SI BESOIN ::::::::::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: commande du programme
 java -jar Redacteur_etats_carte.jar %langue% -map %carte% -out %out% -img %illustration% ^
- -game %game% -mod %mod1% %mod2% %mod3% -sta %nbEtats% -pol %police% %max% ^
- %multicolor% %dateDroite% %dateAn% %dateText%
+ %game% -gameDir %gameDir% -mod %mod1% %mod2% %mod3% -sta %nbEtats% -pol %police% %max% ^
+ %multicolor% %dateDroite% %dateAn% %dateText% -rank %rank%
  
 :: commande pour mettre en pause pour que la console ne se ferme pas à 
 :: la fin de de l'exécution. (utile pour voir les erreurs)
